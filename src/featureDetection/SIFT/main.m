2902083fd66d72ae6562c3f7d8adc224cfd1e403
Paul Joseph <paul.joseph241@gmail.com>
2021-11-29 17:12:41 +0100

    better hyperparam structure

--- src/featureDetection/SIFT/main.m
+++ src/featureDetection/SIFT/main.m
@@ -11,11 +11,15 @@ test				= true;
 test_range			= 100;
 LoadProjectImages
 
-%% hyperparameters
-num_scales 			= 3; 			% 3, 	Scales per octave (num octaves is calculated automatically).
-sigma 				= 1.6;			% 1,
-contrast_threshold	= 0.04;			% 0.04,
-rescale_factor 		= 0.3; 			% 0.3,	Rescaling of the original image for speed.
+%% hyperparameters SIFT
+sift_num_scales 		= 3; 			% 3, 	Scales per octave (num octaves is calculated automatically).
+sift_sigma 				= 1.6;			% 1,
+sift_contrast_threshold	= 0.04;			% 0.04,
+
+%% hyperparameters matching
+match_threshold		= 100;			% 100
+match_max_ratio		= 0.7;			% 0.7
+match_unique		= true;			% true
 
 %% init variable sizes
 valid_kpts	= cell(1, 2);
@@ -28,8 +32,7 @@ for i = img_indices
 	%img = album(:,:,i);  % why does this not worK???? 
     img = im2uint8(rgb2gray(imread(sprintf('../../../datasets/parking/images/img_%05d.png',i))));		% TODO: make me variable
 	% run SIFT Algo to get descriptors and keypoint locations
-	kp = detectSIFTFeatures(img, Sigma=sigma, NumLayersInOctave=num_scales, ContrastThreshold=contrast_threshold);
-	[desc{i}, valid_kpts{i}] = extractFeatures(img, kp.Location);
+	[kpts{i}, desc{i}] = sift(img, sift_num_scales, sift_sigma, sift_contrast_threshold);
 end
 
 %% plot
@@ -39,11 +42,11 @@ for i = img_indices
     img = imread(sprintf('../../../datasets/parking/images/img_%05d.png',i));		% TODO: make me variable
 	imshow(img); hold on;
 	% plot keypoints (u, v coordinates)
-	plot(valid_kpts{i}(:,1), valid_kpts{i}(:,2), 'rx', 'Linewidth', 2);
+	plot(kpts{i}(:,1), kpts{i}(:,2), 'rx', 'Linewidth', 2);
 	% match (skip first frame)
 	if (i > 1)
-		matches = matchFeatures(desc{i}, desc{i-1}, 'MatchThreshold', 100, 'MaxRatio', 0.7, 'Unique', true);
-%		plotMatchesSIFT(matches' ,valid_kpts{i}', valid_kpts{i-1}');	
+		matches = matchFeatures(desc{i}, desc{i-1}, 'MatchThreshold', match_threshold, 'MaxRatio', match_max_ratio, 'Unique', match_unique);
+		plotMatchesSIFT(matches' ,kpts{i}', kpts{i-1}');	
 	end
 	hold off;
 	pause(0.1);
