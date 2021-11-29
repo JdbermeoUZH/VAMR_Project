a5f537a4c9af282829faed53654eded49ae5d47f
Paul Joseph <paul.joseph241@gmail.com>
2021-11-29 17:04:39 +0100

    documentation

--- src/featureDetection/SIFT/sift.m
+++ src/featureDetection/SIFT/sift.m
@@ -1,9 +1,16 @@
-function [kpt_locations] = computeDescriptors( ...
-	image, num_octaves, num_scales, sigma, contrast_threshold, rotation_inv);
+function [kpts, desc] = computeDescriptors(image, num_scales, sigma, contrast_threshold);
+	% @brief: 	given an image return the SIFT keypoints and the coresponding features (descriptors)
+	% 
+	% @param(image)					: grayscale image
+	% @param(num_scales)			: number of scales to use 
+	% @param(sigma)					: sigam value as learned in lecture
+	% @param(contrast_threshold)	: contrast threshold 
+	% 
+	% for more info on hyperparam (because I wasnt relly specific here):
+	% https://ch.mathworks.com/help/vision/ref/siftpoints.html
+	% https://ch.mathworks.com/help/vision/ref/extractfeatures.html#bsxmas0-validPoints
+	% https://ch.mathworks.com/help/vision/ref/detectsiftfeatures.html?searchHighlight=sift&s_tid=srchtitle_sift_3
 
-	image_pyramid 		= computeImagePyramid(image, num_octaves);
-	blurred_images 	= computeBlurredImages(image_pyramid, num_scales, sigma);
-	DoGs 				= computeDoGs(blurred_images);
-	tmp_kpt_locations	= extractKeypoints(DoGs, contrast_threshold);
-	[descriptors, kpt_locations] = computeDescriptors(blurred_images, tmp_kpt_locations, rotation_inv);
+	kp 				= detectSIFTFeatures(image, Sigma=sigma, NumLayersInOctave=num_scales, ContrastThreshold=contrast_threshold);
+	[desc, kpts] 	= extractFeatures(image, kp.Location);
 end
