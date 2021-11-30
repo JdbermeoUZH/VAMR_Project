633593bf527701cd4ae453457bd5bcbcc68530b8
Paul Joseph <paul.joseph241@gmail.com>
2021-11-30 10:56:55 +0100

    try new hyperparams

--- src/setup/LoadHyperParams.m
+++ src/setup/LoadHyperParams.m
@@ -2,9 +2,9 @@
 bootstrap_frames		= [1 3];
 
 %% hyperparameters SIFT
-sift_num_scales 		= 3; 			% 3, 	Scales per octave (num octaves is calculated automatically).
-sift_sigma 				= 1.6;			% 1,
-sift_contrast_threshold	= 0.04;			% 0.04,
+sift_num_scales 		= 4; 			% 3, 	Scales per octave (num octaves is calculated automatically).
+sift_sigma 				= 1.6;			% 1,    1.6 is recommended in paper (TODO: search for link)
+sift_contrast_threshold	= 0.03;			% 0.04, 0.02 gives more features (and still looks acceptable)
 
 %% hyperparameters matching
 match_threshold			= 100;			% 100
