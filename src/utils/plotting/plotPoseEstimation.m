10991753311cee65124249676af3914b3e19633b
paul <paul.joseph241@gmail.com>
2021-12-06 15:59:28 +0100

    use plotCamera

--- src/utils/plotting/plotPoseEstimation.m
+++ src/utils/plotting/plotPoseEstimation.m
@@ -10,19 +10,24 @@ subplot(fig_num,2,1)
 % P is a [4xN] matrix containing the triangulated point cloud (in
 % homogeneous coordinates), given by the function linearTriangulation
 plot3(P_3D(1,:), P_3D(2,:), P_3D(3,:), 'o');
-
+hold on;
 % Display camera pose
 
-plotCoordinateFrame(eye(3),zeros(3,1), 0.8);
-text(-0.1,-0.1,-0.1,'Cam 1','fontsize',10,'color','k','FontWeight','bold');
+%plotCoordinateFrame(eye(3),zeros(3,1), 0.8);
+%text(-0.1,-0.1,-0.1,'Cam 1','fontsize',10,'color','k','FontWeight','bold');
 
-center_cam2_W = -R_C2_W'*T_C2_W;
-plotCoordinateFrame(R_C2_W',center_cam2_W, 0.8);
-text(center_cam2_W(1)-0.1, center_cam2_W(2)-0.1, center_cam2_W(3)-0.1,'Cam 2','fontsize',10,'color','k','FontWeight','bold');
+%center_cam2_W = -R_C2_W'*T_C2_W;
+%plotCoordinateFrame(R_C2_W',center_cam2_W, 0.8);
+%text(center_cam2_W(1)-0.1, center_cam2_W(2)-0.1, center_cam2_W(3)-0.1,'Cam 2','fontsize',10,'color','k','FontWeight','bold');
+pose_cam1 = rigid3d(eye(3), zeros(1,3));
+pose_cam2 = rigid3d(inv(R_C2_W),T_C2_W');
+cam1 = plotCamera('AbsolutePose',pose_cam1,'Opacity',0)
+cam2 = plotCamera('AbsolutePose',pose_cam2,'Opacity',0)
 
 axis equal
 rotate3d on;
 grid
+hold off
 
 % Display matched points
 subplot(fig_num,2,2)
