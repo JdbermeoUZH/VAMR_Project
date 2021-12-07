226cd8ad0b070771d1e27a862755c0f370d72e74
Paul Joseph <paul.joseph241@gmail.com>
2021-12-07 13:37:01 +0100

    return matched keypoints for testing

--- src/tests/bootstrapTest.m
+++ src/tests/bootstrapTest.m
@@ -1,4 +1,4 @@
-function [fig_count] = bootstrapTest(datasets, hyperparameters, fig_count)
+function [fig_count, matched_keypoints_1, matched_keypoints_2] = bootstrapTest(datasets, hyperparameters, fig_count)
     % actually call bootstrap (without plotting anything because later on we also don't 
     % want to plot everything ... this would just get messy)
     [R, T, P_3D, matched_keypoints_1, matched_keypoints_2] = bootstrap(datasets, hyperparameters);
@@ -6,6 +6,6 @@ function [fig_count] = bootstrapTest(datasets, hyperparameters, fig_count)
     figure(fig_count);
     plotBootstrapOutput(P_3D, R, T, ...
         datasets.img0, datasets.img1, ...
-        matched_keypoints_1, matched_keypoints_2);
+        matched_keypoints_1, matched_keypoints_2, fig_count);
     fig_count = fig_count + 1;
 end
