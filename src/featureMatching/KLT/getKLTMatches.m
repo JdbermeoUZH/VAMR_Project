b344ea16ee80da27d590cd48c949710c051e0324
Paul Joseph <paul.joseph241@gmail.com>
2021-12-13 14:56:59 +0100

    thanks for the easy removal instruction

--- src/featureMatching/KLT/getKLTMatches.m
+++ src/featureMatching/KLT/getKLTMatches.m
@@ -4,9 +4,6 @@ function [matched_keypoints_1,matched_keypoints_2, isMatched] = getKLTMatches( .
 %GETKLTMATCHES Summary of this function goes here
 %   Detailed explanation goes here
 
-% TODO: Remove this if at some point we decide not too invert the coords
-keypoints_1 = [keypoints_1(:,2), keypoints_1(:,1)];
-
 KLT_tracker = vision.PointTracker( ...
 'NumPyramidLevels', NumPyramidLevels, ...
 'MaxBidirectionalError', MaxBidirectionalError, ...
@@ -20,10 +17,6 @@ initialize(KLT_tracker, keypoints_1, img0);
 
 matched_keypoints_1 = keypoints_1(isMatched, :);
 matched_keypoints_2 = round(keypoints_2(isMatched, :));
-
-% TODO: Remove this if at some point we decide not too invert the coords
-matched_keypoints_1 = [matched_keypoints_1(:,2), matched_keypoints_1(:,1)];
-matched_keypoints_2 = [matched_keypoints_2(:,2), matched_keypoints_2(:,1)];
 
 end
 
