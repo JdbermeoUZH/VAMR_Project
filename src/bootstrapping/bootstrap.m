e1bf6300c9459c4fc606946674ab3e70a0283568
paul <paul.joseph241@gmail.com>
2021-12-28 19:22:31 +0100

    seperate fund mat estimation function

--- src/bootstrapping/bootstrap.m
+++ src/bootstrapping/bootstrap.m
@@ -21,27 +21,7 @@ function [R, T, P_3D, matched_keypoints_1, matched_keypoints_2] = bootstrap(data
     K       = datasets.K;
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % could also just use hyperparameters.the_thing_to_use in every function 
-    featDetec_algo              = hyperparameters.featDetec_algo;
-    ransac_algo                 = hyperparameters.ransac_algo;
     featDetec_matchType         = hyperparameters.featDetec_matchType;
-
-    % Harris Stuff
-    corner_patch_size           = hyperparameters.corner_patch_size;
-    harris_kappa                = hyperparameters.harris_kappa;
-    num_keypoints               = hyperparameters.num_keypoints;
-    nonmaximum_supression_radius= hyperparameters.nonmaximum_supression_radius;
-    descriptor_radius           = hyperparameters.descriptor_radius;
-    match_lambda                = hyperparameters.match_lambda;
-    
-    % SIFT Stuff
-    num_scales                  = hyperparameters.sift_num_scales;
-    sigma                       = hyperparameters.sift_sigma;
-    contrast_threshold          = hyperparameters.sift_contrast_threshold;
-    
-    % Pairwise Matching Stuff
-    match_threshold             = hyperparameters.match_threshold;
-    match_max_ratio             = hyperparameters.match_max_ratio;
-    match_unique                = hyperparameters.match_unique;
     
     % KLT Matching Stuff
     NumPyramidLevels            = hyperparameters.klt_NumPyramidLevels;           
@@ -73,27 +53,12 @@ function [R, T, P_3D, matched_keypoints_1, matched_keypoints_2] = bootstrap(data
     end
 
     % Estimate the pose change with ransac
-    % choose which ransac algo to use
-    % TODO: make this a function! Might need ransac stuff more often elsewhere
-    if(ransac_algo == "8point")
-        %% Estimate the pose change with 8-point ransac
-        % Get the fundamental matrix
-        % TODO: Come up with a smart number of parameters, particularly for the
-        %       number of trials
-        [F, inliers, ~] = estimateFundamentalMatrix(...
-            matched_keypoints_1, matched_keypoints_2, ...
-            'Method','RANSAC',...
-            'NumTrials', hyperparameters.eightPointNumTrials, ...
-            'DistanceThreshold', hyperparameters.eightPointDistanceThreshold,...
-            'Confidence', hyperparameters.eightPointConfidence);
+    output = getFundamentalMatrix(matched_keypoints_1, matched_keypoints_2, hyperparameters);
+    F               = output.F;
+    matchedInliers1 = output.inlierPts1;
+    matchedInliers2 = output.inlierPts2;
 
-    elseif (ransac_algo == "5point")
-        %TODO
-    end
-    
-    matched_keypoints_1 = matched_keypoints_1(inliers>0, :);
-    matched_keypoints_2 = matched_keypoints_2(inliers>0, :);
-    % Get the essential matrix. We assume K_1=K_2
+    % Get the relative rotaion and translatoin between camera frames. We assume K_1=K_2
     [R, T, P_3D] = recoverPoseFromFundamentalMatrix(...
-        F, K, K,matched_keypoints_1, matched_keypoints_2);
+        F, K, K, matchedInliers1, matchedInliers2);
 end
