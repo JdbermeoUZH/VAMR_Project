0d0d37d461aba9fcf5042df6269ce71dce4228e3
Juan Diego Bermeo <juandiego.bermeoortiz@uzh.ch>
2021-12-29 11:16:32 +0100

    fixed issue with return values in bootstrapping

--- src/tests/continousPoseEstimationTest.m
+++ src/tests/continousPoseEstimationTest.m
@@ -1,22 +1,26 @@
-function [fig_count, matched_keypoints_1, matched_keypoints_2] = continousPoseEstimationTest(datasets, hyperparameters, fig_count)
+function [fig_count] = continousPoseEstimationTest(datasets, hyperparameters, fig_count)
 %CONTINOUSPOSEESTIMATIONTEST Summary of this function goes here
 %   Detailed explanation goes here
     %% Initialize variables
     poses = cell(length(datasets.imgs) + 2);
 
     %% Bootstrap the initial 3D-Point cloud 
-    [R, T, P_3D, matched_keypoints_1, matched_keypoints_2] = bootstrap(datasets, hyperparameters);
+    [R, T, P_3D, ~, matchedInlierPts_2] = bootstrap(datasets, hyperparameters);
     
+    % TODO: Modify the bootstrap function so that it also returns the
+    % not-matched keypoints in image 2, as they are candidates for new
+    % keypooints
+
     % TODO: Include set of asserts for dimensions of each of the outputs inside
     % the function.
 
     % Create the initial state with our second keyframe
-    num_landmarks = length(matched_keypoints_2);
+    num_landmarks = length(matchedInlierPts_2);
 
     T_wc_before = [R, T];
     img_old = datasets.img1; 
     State_before.X = P_3D.';                     % Mx3 array of 3D coordinates of landamarks/keypoints
-    State_before.P = matched_keypoints_2;        % Mx2 array of 2D coordinates of landmarks/keypoints in the keyframe's Image plane
+    State_before.P = matchedInlierPts_2;        % Mx2 array of 2D coordinates of landmarks/keypoints in the keyframe's Image plane
     State_before.C = ones(num_landmarks, 3);     % Mx3 array of 3D coordinates of candidate landamarks/keypoints of non-keyframes
     State_before.F = ones(num_landmarks, 2);     % Mx2 array of 2D coordinates of candidate landmarks/keypoints in the image plane where they were first detected
     State_before.T = ones(num_landmarks, 12);    % Mx12 array of poses of the candidate landmarks/keypoints when they were first located
