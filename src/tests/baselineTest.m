d05a9f2ba40308fea393aaf751efc505d1fe1038
lambdacitizen <91560668+lambdacitizen@users.noreply.github.com>
2022-01-07 02:07:37 +0100

    Add files via upload

--- src/tests/baselineTest.m
+++ src/tests/baselineTest.m
@@ -1,6 +1,6 @@
-function [fig_count] = harrisTest(datasets, hyperparameters, fig_count)
+function [fig_count,allimginf,numtgfis] = baselineTest(datasets, hyperparameters, fig_count)
 
-
+tic
 %This functions currently only calculates local 3D point clouds and the
 %relative positions of the keyframes relative to the precedent keyframe
 
@@ -12,6 +12,10 @@ function [fig_count] = harrisTest(datasets, hyperparameters, fig_count)
 %updating bootstrap will not also modify this code. I have attempted to
 %isolate the pieces of the bootstrapping algorithm I use, so we can just
 %ctrl c-v it
+
+%Parking: 46s, 152 frames studied, 69 KF
+
+%Malaga 1st 300frames: 282s, 135 calculated frames, 59 KF
 
 
 
@@ -104,6 +108,9 @@ function [fig_count] = harrisTest(datasets, hyperparameters, fig_count)
             getKLTMatches(img0, keypoints_1, img1, ...
                           NumPyramidLevels, MaxBidirectionalError, ...
                           MaxIterations, BlockSize);
+        keypoints_2 = nan;
+        descriptors_1 = nan;
+        descriptors_2 = nan;
     else
         % output error
         error('The given feature detection method is not valid');
@@ -119,9 +126,38 @@ function [fig_count] = harrisTest(datasets, hyperparameters, fig_count)
     [R, T, P_3D] = recoverPoseFromFundamentalMatrix(...
         F, K, K, matchedInliers1, matchedInliers2);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Structures containing useful info about each KF
+% Info on each KF is local, in the reference frame of the previous KF
+% (except KF1, which is in its own)
+
+    inf1 = struct("frame",hyperparameters.bootstrap_frames(1),"KP",keypoints_1,...
+    "des",descriptors_1,"KFmatches",matched_keypoints_1,...
+    "framematches",matched_keypoints_1, ...
+    "frontstep",0,"sidestep",0,...
+    "matchedInliers1",matchedInliers1,...
+    "matchedInliers2",matchedInliers2,...
+    "FundamentalMatrix",nan,...
+    "Rotation",eye(3),...
+    "Translation",zeros(3,1),...
+    "ptcloud",P_3D, ...
+    "KF",1);
+
+    inf2 = struct("frame",hyperparameters.bootstrap_frames(2),"KP",keypoints_2,...
+    "des",descriptors_2,"KFmatches",matched_keypoints_2,...
+    "framematches",matched_keypoints_2, ...
+    "frontstep",0,"sidestep",0,...
+    "matchedInliers1",matchedInliers1,...
+    "matchedInliers2",matchedInliers2,...
+    "FundamentalMatrix",nan,...
+    "Rotation",R,...
+    "Translation",T,...
+    "ptcloud",P_3D, ...
+    "KF",1);
+
+bootimginf = [inf1,inf2];
 
 
-%
 nframes = diff(hyperparameters.bootstrap_frames);
 [ysz,xsz] = size(datasets.img0);
 K = datasets.K;
@@ -138,15 +174,21 @@ tgfi = nextkeyframe;
 changeframe = 1;
 imginf = struct;
 img = datasets.imgs{frame};
-matchednumber = length(matchedInliers1);
+% matchednumber = length(matchedInliers1); %%%%%%%%%
 
 while changeframe ~= 0
     [changeframe,imginf] = ValidateKeyframe(hyperparameters,...
-        img,tgfi,changeframe,keypoints_2,descriptors_2,minlat,minfwd,imginf,matchednumber,K,img1);
+                img,tgfi,changeframe,keypoints_2,descriptors_2,minlat,minfwd,imginf,K,img1);
+
+%         img,tgfi,changeframe,keypoints_2,descriptors_2,minlat,minfwd,imginf,matchednumber,K,img1);
     img = datasets.imgs{frame};
 end
 
 allimginf = imginf;
+alltgfis = zeros(length(datasets.imgs)+hyperparameters.bootstrap_frames(2),1);
+alltgfisindx = 1;
+
+%% Iteration
 
 while frame<length(datasets.imgs)
     P_3D = allimginf(end).ptcloud;
@@ -159,7 +201,8 @@ while frame<length(datasets.imgs)
     
     [nextkeyframe,minlat,minfwd] = Baseline(P_3D,T,R,nframes,xsz,ysz,K);
 
-    alltgfis = [alltgfis,nextkeyframe];
+    alltgfis(alltgfisindx) = nextkeyframe;
+    alltgfisindx = alltgfisindx+1;
 
     keyframe = frame;
     i = nextkeyframe;
@@ -172,14 +215,17 @@ while frame<length(datasets.imgs)
     imginf = struct;
     KFimg = img;
     img = datasets.imgs{frame};
-    matchednumber = length(allimginf(end).matchedInliers2);
+%     matchednumber = length(allimginf(end).matchedInliers2); %%%%%%%%%
     KFKP = allimginf(end).KP;
     KFdes = allimginf(end).des;
+    
 
 
     while changeframe ~= 0 && frame<length(datasets.imgs)
         [changeframe,imginf] = ValidateKeyframe(hyperparameters,...
-            img,tgfi,changeframe,KFKP,KFdes,minlat,minfwd,imginf,matchednumber,K,KFimg);
+                        img,tgfi,changeframe,KFKP,KFdes,minlat,minfwd,imginf,K,KFimg);
+
+%             img,tgfi,changeframe,KFKP,KFdes,minlat,minfwd,imginf,matchednumber,K,KFimg);
         if frame > length(datasets.imgs)
             frame = length(datasets.imgs);
 
@@ -187,21 +233,129 @@ while frame<length(datasets.imgs)
         img = datasets.imgs{frame};
     end
 
-    allimginf = [allimginf,imginf];
-
+    if isequal(fieldnames(imginf),fieldnames(allimginf))
+        allimginf = [allimginf,imginf];
+    end
 
 end
 
+alltgfis = nonzeros(alltgfis);
+numtgfis = length(alltgfis);
+
+% Reestablishing correct frame numbering
+loopnums = [allimginf(:).frame];
+loopnums = loopnums + ones(size(loopnums))*hyperparameters.bootstrap_frames(2);
+for index = 1:length(loopnums)
+allimginf(index).frame = loopnums(index);
+end
+
+allimginf = [bootimginf,allimginf];
+
+% numKF = sum([allimginf(:).KF]);%%%%%%%%%%
+
+%% Absolute pose
+
+
+
+rot = [allimginf(:).Rotation];
+
+% rot1 = [allimginf(1).Rotation]; %%%%%%%%%%%%%%
+% rot2 = [allimginf(2).Rotation];
+
+rot3dm = reshape(rot,3,3,[]);
+
+trans = [allimginf(:).Translation];
+
+trans3dm = reshape(trans,3,1,[]);
+
+pose = [rot3dm,trans3dm];
+
+additive = zeros(1,4,size(pose,3));
+additive(1,end,:) = 1;
+
+pose = [pose;additive];
+
+
+
+poseabs = pose;
+for i=2:size(pose,3)
+    poseabs(:,:,i) = poseabs(:,:,i-1)*poseabs(:,:,i);
+end
+
+transform = poseabs;
+poseabs = poseabs(1:3,:,:);
+
+abstrans = poseabs(:,4,:);
+
+
+
+% scatter3(abstrans(1,1,:),abstrans(2,1,:),abstrans(3,1,:))
+% x = abstrans(1,1,:);
+x = abstrans(1,1,:);
+y = abstrans(2,1,:);
+z = abstrans(3,1,:);
+
+
+
+x = x(:);
+y = y(:);
+z = z(:);
+
+figure(fig_count)
+subplot(1,2,1)
+
+plot3(x,y,z,'-ob')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+% set(gca,'DataAspectRatio',[1 .10^-10 10^-10])
+hold on
+scatter3(x(1),y(1),z(1),'r')
+
+hold off
+
+fig_count = fig_count+1;
+
+%% 3D Point Cloud
+
+
+for index = 3:numel(allimginf)
+    relp3di = [allimginf(index).ptcloud];
+    addit = ones(1,length(relp3di));
+    relp3di = transform(:,:,index-1)*[relp3di;addit];
+    relp3di = relp3di(1:3,:);
+    allimginf(i).ptcloud = relp3di;
+end
+
+absp3d = [allimginf(2:end).ptcloud];
+
+subplot(1,2,2)
+scatter3(absp3d(1,:),absp3d(2,:),absp3d(3,:))
+xlabel('x')
+ylabel('y')
+zlabel('z')
+% plot(absp3d(1,:))
+% plot(absp3d(2,:))
+% plot(absp3d(3,:))
+
+% sure = vecnorm(absp3d)
+
+toc
 
 
 
 
-test = 0;
 
 
 
+% Next to do: KLT the intermediary frames
+%if done with KP, we can first try to match only with inliers, and then
+%take more if it doesn't fly
 
+%Make visualisation better
 
+%Right now, the code is setup so trajectory calculation is done at the very
+%end. Should I alter it so that the 
 
 
 
